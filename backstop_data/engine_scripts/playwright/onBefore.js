e42e717dfaae35944810c126dc25b91b644b1a4c
orlandoayeras <orlandomark.ayeras@gmail.com>
2026-06-21 15:48:48 +0800

    Created default files for onBefore and onReady scripts.

--- backstop_data/engine_scripts/playwright/onBefore.js
+++ backstop_data/engine_scripts/playwright/onBefore.js
@@ -1,3 +1,6 @@
+// onBefore.js - custom script to load cookies before each scenario
+// Please see the default onBefore.js in the Playwright engine scripts for an example of how to load cookies from the scenario config.
+
 module.exports = async (page, scenario, viewport, isReference, browserContext) => {
   await require('./loadCookies')(browserContext, scenario);
 };
--- backstop_data/engine_scripts/playwright/onReady.js
+++ backstop_data/engine_scripts/playwright/onReady.js
@@ -1,3 +1,6 @@
+// onReady.js - custom script to perform actions after the page is loaded but before the screenshot is taken
+// Please see the default onReady.js in the Playwright engine scripts for an example of how to interact with the page before the screenshot.
+
 module.exports = async (page, scenario, viewport, isReference, browserContext) => {
   console.log('SCENARIO > ' + scenario.label);
   await require('./clickAndHoverHelper')(page, scenario);
